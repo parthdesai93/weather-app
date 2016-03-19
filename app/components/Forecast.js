0257014242a15eb4f6db31930efd1ac86e823b50
parth desai <psdesai.93@gmail.com>
2016-03-19 00:54:12 -0400

    format weather data

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -1,11 +1,30 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
 
+function parseData(data){
+  console.log("in paerse data");
+  console.log(data);
+  var formattedData = data.map(function(obj){
+    var weather = {};
+    console.log('inmap');
+    weather['humdity'] = obj.humidity;
+    weather['min_temp'] = obj.temp.min;
+    weather['max_temp'] = obj.temp.max;
+    weather['date'] = obj.dt;
+    return weather;
+  })
+  console.log(formattedData);
+}
+
 function Forecast(props){
   return(
-    props.isLoading === true ?
-    <div> Loading </div> :
-    <div>Loaded data </div>
+    props.isLoading === true
+      ? <div> Loading </div>
+      : <div>
+          {console.log(props)}
+          {parseData(props.forecastData)}
+          <h2>Data Loaded</h2>
+        </div>
   )
 }
 
--- app/utils/ForecastHelper.js
+++ app/utils/ForecastHelper.js
@@ -6,7 +6,7 @@ var secrets = require('../secrets/Secrets');
 var APP_ID = secrets.open_weather_api_key;
 
 function getWeather(cityName){
-  return axios.get('http://api.openweathermap.org/data/2.5/forecast/daily?q='+ cityName + '&type=accurate&appid=' + APP_ID + '&units=metric&cnt=5');
+  return axios.get('http://api.openweathermap.org/data/2.5/forecast/daily?q='+ cityName + '&appid=' + APP_ID + '&units=metric&cnt=5');
 }
 
 var helpers = {

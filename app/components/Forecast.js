1e4f05d43bc2ea9d02ae29c37cd26b881c523285
parth desai <psdesai.93@gmail.com>
2016-03-21 01:09:26 -0400

    fix css

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -13,25 +13,25 @@ function Forecast(props){
               <h1 className="text-center">{props.cityName}</h1>
             </div>
             <div className="row">
-              <div className="col-xs-4">
+              <div className="col-xs-12 col-sm-4">
                 <h2> {props.forecastData[0].description}</h2>
                 <h3> {props.forecastData[0].day} , {props.forecastData[0].month + ' ' + props.forecastData[0].date} </h3>
               </div>
-              <div className="col-xs-4">
+              <div className="col-xs-12 col-sm-4">
                 <h2> {props.forecastData[1].description}</h2>
                 <h3> {props.forecastData[1].day} , {props.forecastData[1].month + ' ' + props.forecastData[1].date} </h3>
               </div>
-              <div className="col-xs-4">
+              <div className="col-xs-12 col-sm-4">
                 <h2> {props.forecastData[2].description}</h2>
                 <h3> {props.forecastData[2].day} , {props.forecastData[2].month + ' ' + props.forecastData[2].date} </h3>
               </div>
             </div>
             <div className="row">
-              <div className="col-xs-4">
+              <div className="col-xs-12 col-sm-4">
                 <h2> {props.forecastData[3].description}</h2>
                 <h3> {props.forecastData[3].day} , {props.forecastData[3].month + ' ' + props.forecastData[3].date} </h3>
               </div>
-              <div className="col-xs-4 col-xs-offset-4">
+              <div className="col-xs-12 col-sm-4 col-sm-offset-4">
                 <h2> {props.forecastData[4].description}</h2>
                 <h3> {props.forecastData[4].day} , {props.forecastData[4].month + ' ' + props.forecastData[4].date} </h3>
               </div>
